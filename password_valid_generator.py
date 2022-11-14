cee45ab072aa590113a01f765d3124494e1c68fe
Dmyto Marchenko <d.i.marchenko@student.khai.edu>
2022-11-14 12:25:48 +0200

    password_onlyvalid_generator
    Generate you a random guarded password which is valid on all sites etc.

---
+++ password_valid_generator.py
@@ -0,0 +1,43 @@
+from random import randint
+
+
+def get_random_password():
+    result = ""
+    count = 0
+    while count < 8:
+        random_symbol = chr(randint(40, 126))
+        result = result + random_symbol
+        count = count + 1
+    return result
+
+
+def is_valid_password(password):
+    has_upper = False
+    has_lower = False
+    has_num = False
+    for ch in password:
+        if "A" <= ch <= "Z":
+            has_upper = True
+        elif "a" <= ch <= "z":
+            has_lower = True
+        elif "0" <= ch <= "9":
+            has_num = True
+    if len(password) == 8 and has_upper and has_lower and has_num:
+        return True
+    return False
+
+
+def get_password():
+    result = 0
+    password = get_random_password()
+    for i in range(0, 100):
+        if is_valid_password(password) == True:
+            result = password
+            return result
+        else:
+            password = get_random_password()
+            continue
+
+
+
+print(get_password())
